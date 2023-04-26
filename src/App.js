e9d6f5074d40e18277776e159d1e5e781d72d05c
AsmitaShivajiTagad <asmitatagad07@gmail.com>
2023-04-27 02:07:55 +0530

    Login Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Login2 from "./pages/login/Login2";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Register />} />
+        <Route path="/loginto" element={<Login2 />} />
       </Routes>
     </div>
   );
